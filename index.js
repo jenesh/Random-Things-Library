5a5ee900f39171fa409f417bc15485d3db375faf
jenesh <jenesh@codecademy.com>
2023-06-19 10:28:24 -0400

    add random string generator by char size

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-function arrayGenerator(min, max, arrSize) {
-	let arr = [];
-	for (let i = 0; i < arrSize / 2; i++) {
-			const positiveNumber = randomNumber(max);
-			const negativeNumber = randomNumber(min);
+import { arrayNumGenerator } from "./lib/arrays.js";
+import randomNumber from "./lib/util.js";
 
-			arr.push(positiveNumber);
-			arr.push(negativeNumber);
-	}
-	return arr;
-}
-
-function randomNumber(num) {
-	return Math.floor(Math.random() * num);
-}
-
-module.exports = {
-	arrayGenerator,
-	randomNumber
-}
+export { arrayNumGenerator, randomNumber };
